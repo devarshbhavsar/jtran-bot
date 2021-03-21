2b8d65f5ca79eaea2b634987fe8b22be7c557c17
Devarsh Bhavsar <Devarsh.bhavsar01@gmail.com>
2021-03-21 16:17:29 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require('/config.json');
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('message', async message => {
+
+    const specificUsers = ['77219296584933376', '746206965285388370', '476609723769159680'];
+    const specificUsers2 = ['354789485449576449'];
+    if(specificUsers.includes(message.author.id)) {
+        var voiceChannel = message.member.voice.channel;
+        if(voiceChannel) {
+            await message.guild.member(message.author.id).voice.setChannel('821102487029022820');
+            voiceChannel = message.member.voice.channel;
+            await voiceChannel.join().then(async connection => {
+                const dispatcher = await connection.play('./audio.mp3');
+                await dispatcher.on("finish", async end => { await voiceChannel.leave()});
+            }).catch(console.error);
+        }
+    }
+
+
+    if(specificUsers.includes(message.author.id)) {
+        const reactionEmoji = message.guild.emojis.cache.find(emoji => emoji.name === 'ron');
+    	message.react(reactionEmoji);
+    }
+    const reactionEmoji = message.guild.emojis.cache.find(emoji => emoji.name === 'jtran');
+	message.react(reactionEmoji);
+});
+
+client.login(config.token)

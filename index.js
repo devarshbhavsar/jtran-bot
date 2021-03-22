bae699b275274fa191e337e9abf59cfd4deea5de
Devarsh Bhavsar <Devarsh.bhavsar01@gmail.com>
2021-03-22 13:50:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,15 +9,22 @@ client.on('message', async message => {
 
     const specificUsers = ['77219296584933376', '746206965285388370', '476609723769159680'];
     const specificUsers2 = ['354789485449576449'];
+    var count = 3;
     if(specificUsers.includes(message.author.id)) {
-        var voiceChannel = message.member.voice.channel;
-        if(voiceChannel) {
-            await message.guild.member(message.author.id).voice.setChannel('821102487029022820');
-            voiceChannel = message.member.voice.channel;
-            await voiceChannel.join().then(async connection => {
-                const dispatcher = await connection.play('./audio.mp3');
-                await dispatcher.on("finish", async end => { await voiceChannel.leave()});
-            }).catch(console.error);
+        if (count===0){
+            var voiceChannel = message.member.voice.channel;
+            if(voiceChannel) {
+                await message.guild.member(message.author.id).voice.setChannel('821102487029022820');
+                voiceChannel = message.member.voice.channel;
+                await voiceChannel.join().then(async connection => {
+                    const dispatcher = await connection.play('./audio.mp3');
+                    await dispatcher.on("finish", async end => { await voiceChannel.leave()});
+                }).catch(console.error);
+            }
+            count = 3;
+        }
+        else {
+            count = count - 1;
         }
     }
 

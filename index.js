053fa0945e2c1ef9a8b4b5b76e0d9cbfc4ef6dc3
Devarsh Bhavsar <Devarsh.bhavsar01@gmail.com>
2021-03-22 13:49:11 -0400

    ap ib server conversion

--- index.js
+++ index.js
@@ -7,26 +7,30 @@ client.once('ready', () => {
 
 client.on('message', async message => {
 
-    const specificUsers = ['77219296584933376', '746206965285388370', '476609723769159680'];
+    const specificUsers = ['342148761776029696', '280070350526480384', '162326384025272320', '331217736417935361', '162227349046165505', '552231236924473394', '342896889387155457', '267061060366761984', '590761165122961418', '77219296584933376'];
     const specificUsers2 = ['354789485449576449'];
-    if(specificUsers.includes(message.author.id)) {
+    if(specificUsers2.includes(message.author.id)) {
+        var count = 5;
         var voiceChannel = message.member.voice.channel;
         if(voiceChannel) {
-            await message.guild.member(message.author.id).voice.setChannel('821102487029022820');
-            voiceChannel = message.member.voice.channel;
-            await voiceChannel.join().then(async connection => {
-                const dispatcher = await connection.play('./audio.mp3');
-                await dispatcher.on("finish", async end => { await voiceChannel.leave()});
-            }).catch(console.error);
+            if(count===0) {
+                await message.guild.member(message.author.id).voice.setChannel('795192149381087233');
+                voiceChannel = message.member.voice.channel;
+                await voiceChannel.join().then(async connection => {
+                    const dispatcher = await connection.play('./audio.mp3');
+                    await dispatcher.on("finish", async end => { await voiceChannel.leave()});
+                }).catch(console.error);
+                count = count - 1;
+            }
         }
     }
 
 
     if(specificUsers.includes(message.author.id)) {
-        const reactionEmoji = message.guild.emojis.cache.find(emoji => emoji.name === 'ron');
+        const reactionEmoji = message.guild.emojis.cache.find(emoji => emoji.name === 'bruh');
     	message.react(reactionEmoji);
     }
-    const reactionEmoji = message.guild.emojis.cache.find(emoji => emoji.name === 'jtran');
+    const reactionEmoji = message.guild.emojis.cache.find(emoji => emoji.name === 'pog');
 	message.react(reactionEmoji);
 });
 
